dbd2f1a524261c5ba8f658e5c5a31b7bff289b6a
João <silva.joao1@outlook.com.br>
2025-02-07 00:40:19 -0300

    feat: widget structure

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { UploadWidget } from "./components/upload-widget";
 
 export function App() {
   return  (
-    <main className="h-dvh">
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
      <UploadWidget />
     </main>
   )
