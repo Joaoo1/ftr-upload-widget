69b92bf16180a6204dedc3165bca1eb9c402c6d7
João <silva.joao1@outlook.com.br>
2025-02-10 20:31:38 -0300

    feat: add Button component and integrate with UploadWidgetHeader

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,12 +1,13 @@
 import { Minimize2 } from "lucide-react";
+import { Button } from "./ui/button";
 
 export function UploadWidgetHeader() {
-    return (
-        <div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex item-center justify-between">
-            <span className="text-sm font-medium">Upload files</span>
-            <button>
-                <Minimize2 strokeWidth={1.5} className="size-4" />
-            </button>
-        </div>
-    )
-}   
+	return (
+		<div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between">
+			<span className="text-sm font-medium">Upload files</span>
+			<Button size="icon" className="-mr-2">
+				<Minimize2 strokeWidth={1.5} className="size-4" />
+			</Button>
+		</div>
+	);
+}
